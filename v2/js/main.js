e7db1722e77eee6a76a5defaeb8adfa36962e082
Sander Shi <sander.hacker@hotmail.com>
2018-03-18 16:52:01 -0400

    Version 2 update

--- v2/js/main.js
+++ v2/js/main.js
@@ -6,12 +6,26 @@
 
 $(document).ready(function() {
     // Initial Animations
-    $(".image-overlay").delay(500).animate({"opacity": "0.75"}, 500);
-    $(".landing-page-content").css("display", "flex").hide().delay(1000).fadeIn(1000);
+    $(".fixed-background-overlay").delay(800).animate({"opacity": "0.75"}, 800);
+    $(".landing-page-content").css("display", "flex").hide().delay(1600).fadeIn(1200);
 
-    // Navbar Change on Scroll
+    // Scrolling Changes
     $(document).scroll(function() {
+        // Show Navigation Bar
         $(".navbar").toggleClass("scrolled-navbar", $(this).scrollTop() > 20);
+
+        // Fade-in Text
+        var $about = $(".about-me-content");
+        var $hobbies = $(".hobbies-content");
+        var bottom_of_about = $about.offset().top + 200;
+        var bottom_of_hobbies = $hobbies.offset().top + 200;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        if (bottom_of_window > bottom_of_about) {
+            $about.animate({opacity: 1}, 800);
+        }
+        if (bottom_of_window > bottom_of_hobbies) {
+            $hobbies.animate({opacity: 1}, 800);
+        }
     });
 
     // Smooth Scrolling
